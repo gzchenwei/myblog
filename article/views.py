a785970de682fbf852c10e80b438590253ef12da
chenwei <wchen@coremail.cn>
2017-03-20 15:06:45 +0800

    update

--- article/views.py
+++ article/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import *
 from article.models import Article
 from django.http import HttpResponse
 from datetime import datetime
+post_list = Article.objects.all()[::-1]
 
 def showall(request):
-    post_list = Article.objects.all()[::-1]
+#    post_list = Article.objects.all()[::-1]
     return render(request, 'home.html', {'post_list' : post_list})
 
 def archives(request):
@@ -20,9 +21,10 @@ def archives(request):
 def detail(request, id):
     try:
         post = Article.objects.get(id=str(id))
+#        post_list = Article.objects.all()[::-1]
     except Article.DoesNotExist:
         raise Http404
-    return render(request, 'post.html', {'post' : post})	
+    return render(request, 'post.html', {'post' : post,'post_list': post_list})
 
 def search_tag(request, tag) :
     try:
