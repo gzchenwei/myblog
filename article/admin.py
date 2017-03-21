51e0192f59c6285bf7105ae8096eb33eef1fbc04
chenwei <chenwei.gz@qq.com>
2017-03-21 10:10:39 +0800

    modify admin.py add display fields

--- article/admin.py
+++ article/admin.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 
 from article.models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'date_time')
+
+admin.site.register(Article, ArticleAdmin)
 
 
