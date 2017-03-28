3cf5ac5c6b3315e353c4aef5d4bdd810fe76cc73
chenwei <chenwei.gz@qq.com>
2017-03-28 16:52:51 +0800

    update

--- myblog/views.py
+++ myblog/views.py
@@ -11,18 +11,17 @@ def about(request):
     return  render(request,'about.html',{'post_list': post_list})
 
 def index(request):
-    return render(request,'index.html')
-
-def mylogin(request):
     if request.method == 'POST':
         username = request.POST.get('user_id')
         password = request.POST.get('pwd')
         t = authenticate(username=username, password=password)
         if t is not None:
             login(request, t)
-            response = HttpResponseRedirect('/blog')
-            return response
+            #response = HttpResponseRedirect('/blog/?next=%s' % request.path)
+            #return response
+            return redirect(redirect_to)
         else:
             errors="pls check your user and password"
             return render_to_response('index.html',{'errors': errors})
-
+    else:
+        return render(request,'index.html')
