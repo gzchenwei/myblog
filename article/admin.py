e11a36550aa907b294e2fda42def72bdac4f8f84
chenwei <chenwei.gz@qq.com>
2017-04-25 20:16:50 +0800

    update

--- article/admin.py
+++ article/admin.py
@@ -6,7 +6,15 @@ from article.models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'date_time','author','update_time')
+#    class Media:
+#        js = (
+#            'js/editor/kindeditor-4.1.7/kindeditor-all.js',
+#            'js/editor/kindeditor-4.1.7/lang.zh_CN.js',
+#            'js/editor/kindeditor-4.1.7/config.js',
+#        )
 
 admin.site.register(Article, ArticleAdmin)
+
+
 
 
